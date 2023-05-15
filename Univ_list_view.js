e9f1a92571703e8c53969091756bd366a7137805
nanowater <79461476+almini718@users.noreply.github.com>
2023-05-15 15:44:34 +0900

    Update Univ_list_view.js

--- Univ_list_view.js
+++ Univ_list_view.js
@@ -2,7 +2,7 @@
 
 function load_Univ_list() {
   const arr = [
-    ["부산대", "/Busan_Univ/Busan.html"],
+    ["부산대", "https://almini718.github.io/change_score_calculator/Busan_Univ/Busan.html"],
     ["부경대", "https://almini718.github.io/change_score_calculator/Pukyong_Univ/Pukyong.html"],
     ["한국해양대", "https://almini718.github.io/change_score_calculator/Korea_Maritime&Ocean_Univ/Korea_M&O.html"],
   ];
